00017b98f073bf7890f8646925a109a4df8334ea
“Gihae <designbygihae@gmail.com>
2022-02-11 14:11:04 -0500

    update

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -45,22 +45,4 @@ const SignUp = (props) => {
 }
 
 
-const SearchResults= (props) => {
-
-    return (
-        <div className='searchdiv'>
-          <input
-            type="text"
-            name="Search"
-            value={props.value}
-            placeholder="Search Restaurants"
-            onChange={props.onChange}
-          ></input>
-          <button className='searchbtn' type="submit">Search</button>
-        </div>
-      )
-    
-}
-
-
 export default SignUp
