e8f8edfb2cc5eb12bebdfa8ad04e654a4b1e0a12
“Gihae <designbygihae@gmail.com>
2022-02-11 14:19:57 -0500

    update

--- client/src/store/actions/UserAction.js
+++ client/src/store/actions/UserAction.js
@@ -81,19 +81,3 @@ export const UserToUpdate = (id, form) => {
     }
 }
 
-const SearchResults= (props) => {
-
-    return (
-        <div className='searchdiv'>
-          <input
-            type="text"
-            name="Search"
-            value={props.value}
-            placeholder="Search Restaurants"
-            onChange={props.onChange}
-          ></input>
-          <button className='searchbtn' type="submit">Search</button>
-        </div>
-      )
-    
-}
