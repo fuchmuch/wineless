a6b0867f76261518f65c7a69f1b7819d30bc55e0
“Gihae <designbygihae@gmail.com>
2022-02-11 14:12:00 -0500

    update

--- client/src/Services/index.js
+++ client/src/Services/index.js
@@ -3,3 +3,21 @@ import axios from 'axios'
 const Client = axios.create({ baseURL: 'http://localhost:8000/api' })
 
 export default Client
+
+
+const SearchResults= (props) => {
+
+    return (
+        <div className='searchdiv'>
+          <input
+            type="text"
+            name="Search"
+            value={props.value}
+            placeholder="Search Restaurants"
+            onChange={props.onChange}
+          ></input>
+          <button className='searchbtn' type="submit">Search</button>
+        </div>
+      )
+    
+}
