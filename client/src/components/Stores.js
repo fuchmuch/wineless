759c211124a13396a73f07fa2913410cc68b4815
“Gihae <designbygihae@gmail.com>
2022-02-11 14:10:29 -0500

    update

--- client/src/components/Stores.js
+++ client/src/components/Stores.js
@@ -35,22 +35,6 @@ const Details = (props) => {
 }
 
 
-const SearchResults= (props) => {
-
-    return (
-        <div className='searchdiv'>
-          <input
-            type="text"
-            name="Search"
-            value={props.value}
-            placeholder="Search Restaurants"
-            onChange={props.onChange}
-          ></input>
-          <button className='searchbtn' type="submit">Search</button>
-        </div>
-      )
-    
-}
 
 
 
