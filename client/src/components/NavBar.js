9686fb6c71fc967af666026d32fceb339fc8aed8
“Gihae <designbygihae@gmail.com>
2022-02-11 14:08:56 -0500

    update

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,5 +12,25 @@ const NavBar = () => {
       </div>
     )
 }
+import React from 'react';
+
+const SearchResults= (props) => {
+
+    return (
+        <div className='searchdiv'>
+          <input
+            type="text"
+            name="Search"
+            value={props.value}
+            placeholder="Search Restaurants"
+            onChange={props.onChange}
+          ></input>
+          <button className='searchbtn' type="submit">Search</button>
+        </div>
+      )
+    
+}
+
+export default SearchResults;
 
 export default NavBar
