fbf02a9f791b735203c95a8be01146626d088e40
“Gihae <designbygihae@gmail.com>
2022-02-11 14:10:14 -0500

    update

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -136,23 +136,6 @@ const Profile = (props) => {
 }
 
 
-const SearchResults= (props) => {
-
-    return (
-        <div className='searchdiv'>
-          <input
-            type="text"
-            name="Search"
-            value={props.value}
-            placeholder="Search Restaurants"
-            onChange={props.onChange}
-          ></input>
-          <button className='searchbtn' type="submit">Search</button>
-        </div>
-      )
-    
-}
-
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile)
--- client/src/components/Stores.js
+++ client/src/components/Stores.js
@@ -35,5 +35,23 @@ const Details = (props) => {
 }
 
 
+const SearchResults= (props) => {
+
+    return (
+        <div className='searchdiv'>
+          <input
+            type="text"
+            name="Search"
+            value={props.value}
+            placeholder="Search Restaurants"
+            onChange={props.onChange}
+          ></input>
+          <button className='searchbtn' type="submit">Search</button>
+        </div>
+      )
+    
+}
+
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(Details)
