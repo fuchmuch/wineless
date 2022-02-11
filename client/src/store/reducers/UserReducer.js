7ea96b5040fc25560554b7770eecc4bbeee117af
“Gihae <designbygihae@gmail.com>
2022-02-11 14:17:42 -0500

    update

--- client/src/store/reducers/UserReducer.js
+++ client/src/store/reducers/UserReducer.js
@@ -25,21 +25,4 @@ const UserReducer = (state = initialState, action) => {
     }
 }
 
-const SearchResults= (props) => {
-
-    return (
-        <div className='searchdiv'>
-          <input
-            type="text"
-            name="Search"
-            value={props.value}
-            placeholder="Search Restaurants"
-            onChange={props.onChange}
-          ></input>
-          <button className='searchbtn' type="submit">Search</button>
-        </div>
-      )
-    
-}
-
 export default UserReducer
