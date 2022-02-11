22ac101fa53f7ac1dfbc49498075bb4e8923b333
“Gihae <designbygihae@gmail.com>
2022-02-11 14:13:13 -0500

    update

--- client/src/globals/index.js
+++ client/src/globals/index.js
@@ -3,3 +3,23 @@ import Client from '../services/index'
 process.env.NODE_ENV === 'production'
   ? `${window.location.origin}/api`
   : `${Client}`
+
+
+
+
+const SearchResults= (props) => {
+
+    return (
+        <div className='searchdiv'>
+          <input
+            type="text"
+            name="Search"
+            value={props.value}
+            placeholder="Search Restaurants"
+            onChange={props.onChange}
+          ></input>
+          <button className='searchbtn' type="submit">Search</button>
+        </div>
+      )
+    
+}
