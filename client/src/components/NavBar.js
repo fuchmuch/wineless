bad6c0f51eb2e0df678b2b4ff0aee7fd260b2659
“Gihae <designbygihae@gmail.com>
2022-02-13 22:24:58 -0500

    update;

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,53 @@
 import React from "react";
 import { Link, BrowserRouter as Router } from "react-router-dom";
+import { useHistory, Link } from 'react-router-dom';
+import { BiSearch } from "react-icons/bi"
+import Search from './SearchResults';
+import SearchResults from './SearchResults';
+// import './styles/css';
+import About from './About';
 
-const NavBar = () => {
+
+function Nav() {
+    const [inputValue, setInputValue] = React.useState('')
+    let history = useHistory()
+    console.log(inputValue,'<InputValue')
+  
+
     return (
-        <Router>
-      <div >
-          <nav className="nav-bar">
-            <Link to='/Home' id="nav-e">  </Link>
-            <Link to='/Stores' id="nav-e"> Stores </Link>
-            <Link to='/Users' id="nav-e"> Users </Link>
-           
-          </nav>
-          
-      </div>
-      </Router>
-    )
-}
-
-
-export default NavBar
+  
+      <nav className="navbar">
+        <BiSearch className='searchicon'/>
+        <form className='searchBar' onSubmit={onSubmit}>
+        <SearchResults
+        onChange={ (e) => {
+          setInputValue(e.target.value)}
+        }
+        value={inputValue}
+         />
+      </form>
+  
+      <Link to="/About">
+       <button className="navbutton" type="button">
+           About
+       </button>
+      </Link>  
+  
+  
+        <Link to="/Login">
+       <button className="navbutton" type="button">
+           Login
+       </button>
+   </Link>
+  
+        <Link to="/Home">
+        <MdOutlineAccountCircle className= 'accounticon' size='4rem'/>
+        </Link>
+  
+          </nav >
+       
+  
+    );
+  }
+  
+  export default NavBar;
